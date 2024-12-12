9afff3135c00bc1472c914575df0061b3e61411e
Xingjian Zhang <99701420+ZhangXJ199@users.noreply.github.com>
2024-12-12 14:37:39 +0800

    Update __init__.py

--- tinyllava/model/__init__.py
+++ tinyllava/model/__init__.py
@@ -3,5 +3,4 @@ from .connector import *
 from .vision_tower import *
 from .configuration_tinyllava import *
 from .modeling_tinyllava import *
-from .convert_legecy_weights_to_tinyllavafactory import *
 from .load_model import *
