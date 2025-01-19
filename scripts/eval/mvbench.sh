ac924390d2afa90abe69b61b1f0cad8af194b3aa
Xingjian Zhang <99701420+ZhangXJ199@users.noreply.github.com>
2025-01-19 10:51:03 +0800

    Update mvbench.sh

--- scripts/eval/mvbench.sh
+++ scripts/eval/mvbench.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MODEL_PATH="/data/vlm/zxj/result/llava_video_factory/tiny-llava-phi-2-siglip-so400m-patch14-384-base-finetune"
-MODEL_NAME="tiny-llava-phi-2-siglip-so400m-patch14-384-base-finetune"
+MODEL_PATH="/data/vlm/zxj/result/llava_video_factory-12.16/tiny-llava-Qwen2.5-3B-siglip-so400m-patch14-384-base-finetune"
+MODEL_NAME="tiny-llava-Qwen2.5-3B-siglip-so400m-patch14-384-base-finetune"
 EVAL_DIR="/data/vlm/zxj/data/MVBench"
 
 # num_frame=-1 means 1fps
@@ -11,6 +11,6 @@ python -m tinyllava.eval.eval_mvbench \
     --question-file $EVAL_DIR/json \
     --answers-file $EVAL_DIR/answers/$MODEL_NAME.jsonl \
     --temperature 0 \
-    --conv-mode phi \
+    --conv-mode qwen2_base \
     --num_frame 16 \
     --max_frame 16 
