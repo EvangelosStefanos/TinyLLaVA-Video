5a8ad5218124ee89accca91f30e38a478a2bb34f
EvanStefan <steve@actlife.gr>
2025-09-25 14:52:55 +0300

    Update mmvu evaluation

--- scripts/eval/mmvu.sh
+++ scripts/eval/mmvu.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-MODEL_PATH="/data/vlm/zxj/result/TinyLLaVA-Video-Group-1fps-512"
-MODEL_NAME="TinyLLaVA-Video-Group-1fps-512"
-EVAL_DIR="/data/vlm/zxj/data/MMVU"
+MODEL_PATH="Zhang199/TinyLLaVA-Video-Qwen2.5-3B-Group-16-512"
+MODEL_NAME="TinyLLaVA-Video-Qwen2.5-3B-Group-16-512"
+EVAL_DIR="/app/data/dataset/eval/MMVU"
 
 # num_frame=-1 means 1fps
 python -m tinyllava.eval.eval_mmvu \
     --model_path $MODEL_PATH \
     --image_folder $EVAL_DIR \
     --question_file $EVAL_DIR/validation.json \
-    --answers_file $EVAL_DIR/answers/$MODEL_NAME.jsonl \
+    --answers_file /app/output/eval/mmvu/answers/$MODEL_NAME.jsonl \
     --temperature 0 \
     --conv_mode qwen2_base \
-    --num_frame -1 \
-    --max_frame 64 
+    --num_frame 16 \
+    --max_frame 16
